839b9455182febb298d8caf17eb2ba12caf01610
Ali Aryani <lenesis@proton.me>
2025-08-01 05:48:54 -0400

    integers

---
+++ 01-vars-slicing-typecasting/04-integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# integers are simple, 
+# you can do math with them easily
+# the only thing that you need to be careful about,
+# is that a string and an integer are not the same
+
+number = 12356
+number2 = 65321
+number3 = number + number2
+print (number3)
+
+# about the string that I told you earlier,
+string1 = "750"
+# this is not equal to
+notstring1 = 750
+# if you try to do math with string1
+# errors will start rising ...
