b3aae338936996b953e42cc49517c97eee837ae0
Ali Aryani <lenesis@proton.me>
2025-08-01 06:17:42 -0400

    booleans

---
+++ 01-vars-slicing-typecasting/06-booleans.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Boolean means True or False
+# I assume you already know that
+alive = True
+
+if alive:
+    print ("I'm alive!")
+else:
+    print ("I'm dead. talking dead!")
+
+# And that's it
