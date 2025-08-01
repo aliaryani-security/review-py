c7d3072e47378814e76816a91d501855d7664524
Ali Aryani <lenesis@proton.me>
2025-08-01 06:12:27 -0400

    floats

---
+++ 01-vars-slicing-typecasting/05-floats.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# let's continue with floating-point numbers
+a = 2.3
+# that's it !
+# and you can convert int to float easily
+b = 7
+print (type(b))
+# and now
+c = b +0.5
+print (type(c))
