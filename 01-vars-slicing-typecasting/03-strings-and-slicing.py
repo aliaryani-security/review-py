e24012f5705c8e948385856f12ce03d5d23c493b
Ali Aryani <lenesis@proton.me>
2025-08-01 05:15:11 -0400

    string intro

---
+++ 01-vars-slicing-typecasting/03-strings-and-slicing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# unlike low level languages,
+# strings are not a nightmare here!
+
+# let's write an <a> html tag 
+tag = '<a href="https://github.com/aliaryani-security">Github page</a>'
+# as you can see, using single quote or double quote
+# does not matter, they both can do the job
+# but be careful not to ruin your own code !
+# in the example above, we cannot use double
+# quotes, since we have a pair of double quotes
+# right in the middle of our string! in href value
