d6579c8b4fbf5648379cdea39eb9b0b83a68fcef
Ali Aryani <lenesis@proton.me>
2025-08-02 17:21:12 -0400

    module import

---
+++ 05-modules-and-web-requests/01-importing-modules.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#simply import what you want, there's nothing weird about it
+
+import random
+import time
+
+num = random.randrange(1,10)
+time.sleep (num)
+print ("slept for ",num, "seconds!")
