b718baafa78cee4baef338a6239a279b5773724f
Ali Aryani <lenesis@proton.me>
2025-08-02 17:03:37 -0400

    added comments

--- 03-loops-logic-userinput/02-conditionals.py
+++ 03-loops-logic-userinput/02-conditionals.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
+# by using the input() you can ask for user input:
 age = int (input("How old are you? "))
 
+# and these are how conditions work
 if age < 16:
     print ('go play with your toys child!')
 elif 16 <= age < 18 :
